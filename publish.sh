17aefcd08b687448b7591a36a08b662f83ad4d38
Sergey Mamyan <sergey.mamyan@gmail.com>
2017-08-12 16:13:36 +0400

    Published packages to 0.0.3 version

---
+++ publish.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+npm publish ./node_modules/@ecmal/runtime
+npm publish ./node_modules/@ecmal/react
+npm publish ./node_modules/@ecmal/node
+npm publish ./node_modules/@ecmal/http
+npm publish ./node_modules/@ecmal/gapi
